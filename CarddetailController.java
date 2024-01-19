6c7c5106c0e1d4fcc11bb2bb5459faec9fb806a7
DoThiMay <145520544+DoThiMay@users.noreply.github.com>
2024-01-19 20:53:32 +0700

    Update CarddetailController.java

--- CarddetailController.java
+++ CarddetailController.java
@@ -21,7 +21,27 @@ public class CarddetailController {
 	 @RequestMapping(value = "/getCarddetailByCardID/{cardID}", method = RequestMethod.GET)
 	    public String getCarddetailByCardID(ModelMap modelMap, @PathVariable String cardID) {
 	        Iterable<Carddetail> carddetail = carddetailRespository.findByCardID(cardID);
+		for (Carddetail carddetail2 : carddetail) {
+			carddetail2.setCardID("C100");
+		}
 	        modelMap.addAttribute("carddetail", carddetail);
+		modelMap.addAttribute("carddetail1", new Carddetail()); 
 	        return "carddetailList"; //"productList.jsp"
 	    }
+	//	 ../../carddetail/getUpdateStatus
+	@RequestMapping(value = "/getUpdateStatus/{bookID}", method = RequestMethod.POST)
+	public String getUpdateStatus(ModelMap modelMap, @Validated @ModelAttribute("carddetail1") Carddetail carddetail,
+			BindingResult BindingResult, @PathVariable String bookID) {
+		Optional<Carddetail> foundCarddetail = carddetailRespository.findById(carddetail.getBookID());
+		Carddetail foundCarddetail1 = foundCarddetail.get();
+		if (foundCarddetail.isPresent()) {
+			if (foundCarddetail1.getStatus().equals("NY")) {
+				foundCarddetail1.setStatus("Returned");
+			} else {
+				foundCarddetail1.setStatus("NY");
+			}
+			carddetailRespository.save(foundCarddetail1);
+		}
+		return "redirect:/carddetail/getCarddetailByCardID/" + foundCarddetail1.getCardID();
+	}
 }
