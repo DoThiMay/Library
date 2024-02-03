d3dc395840334b35af20c91ecd0d46a9392363d7
DoThiMay <145520544+DoThiMay@users.noreply.github.com>
2024-02-03 08:59:48 +0700

    Update LibraryCardController.java

--- src/main/java/com/springmvc/library1/controllers/LibraryCardController.java
+++ src/main/java/com/springmvc/library1/controllers/LibraryCardController.java
@@ -20,5 +20,31 @@ public String getAllUser(ModelMap modelMap) {
     modelMap.addAttribute("librarycard", librarycard);
     return "librarycard"; //"productList.jsp"
 }
+@Autowired
+private UserRespository userRespository;
+@RequestMapping(value = "/insertLibrarycard", method = RequestMethod.GET)
+public String insertLibrarycard(ModelMap modelMap) {
+	modelMap.addAttribute("librarycard", new Librarycard());
+	modelMap.addAttribute("user", userRespository.findAll());
+	return "insertLibrarycard";
 
+}
+@RequestMapping(value = "/insertLibrarycard", method = RequestMethod.POST)
+public String insertLibrarycard(ModelMap modelMap, @Validated @ModelAttribute("librarycard") Librarycard librarycard,
+		BindingResult BindingResult // validation
+) {
+	if (BindingResult.hasErrors()) {
+		return "insertLibrarycard";
+	}try {
+		
+		String uuid = String.format("%040d", new BigInteger(UUID.randomUUID().toString().replace("-", ""), 16));
+		String cardIdRandom = uuid.substring(uuid.length() - 4);
+		librarycard.setCardID(cardIdRandom);
+		librarycardRespository.save(librarycard);
+		return "redirect:/librarycard/getAllLibrarycard";
+	} catch (Exception e) {
+		modelMap.addAttribute("error", e.toString());
+		return "insertLibrarycard";
+	}
+}
 }
