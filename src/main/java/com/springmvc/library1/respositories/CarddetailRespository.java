2a7848ba07224a1ea1fa12b06b21de279b7d9543
DoThiMay <145520544+DoThiMay@users.noreply.github.com>
2024-02-03 09:03:28 +0700

    Update CarddetailRespository.java

--- src/main/java/com/springmvc/library1/respositories/CarddetailRespository.java
+++ src/main/java/com/springmvc/library1/respositories/CarddetailRespository.java
@@ -1,9 +1,17 @@
 package com.springmvc.library1.respositories;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.springmvc.library1.models.Carddetail;
 
 public interface CarddetailRespository extends CrudRepository<Carddetail, String>{
-
+  @Query(value = "SELECT COUNT(carddetails.status) as StatusCount FROM Book Inner Join carddetails ON Book.bookID= carddetails.bookID Where carddetails.status=?2 and Book.bookID=?1 GROUP BY Book.bookID", nativeQuery = true)
+	Integer getAvaiableBookNumber(String bookID, String Status);
+	
+	Iterable<Carddetail> findByCardID(String cardID);
+	
+	@Query(value = "Select * from carddetails where cardID = ?1 and bookID = ?2", nativeQuery = true)	
+	Optional<Carddetail> findBook(String cardID, String bookID);
 }
